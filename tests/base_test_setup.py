8722e618fb6eb99f6bbd09cda8a3f43ac2e2ee12
georgreen <georgreen.ngunga@andela.com>
2017-07-05 15:31:34 +0300

    [Feat: #147220365] Implement authentication Tests - Implemnted tests for login and token authentication

--- tests/base_test_setup.py
+++ tests/base_test_setup.py
@@ -1,9 +1,10 @@
+import json
 from unittest import TestCase
 
 from flask import current_app
 
 from app.base import database, new_app
-from app.models import bucketlist, user
+from app.models import bucketlist, profile, user
 
 
 class BaseTestCase(TestCase):
@@ -12,13 +13,28 @@ class BaseTestCase(TestCase):
         self.app = new_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
+        database.drop_all()
         database.create_all()
 
-        self.new_user = user.User("crimson", "password", "crimson@gmail.com")
+        self.registerdata = {
+            "username": "Friedrich Nietzscher",
+            "email": "Nietzschern@gmail.com",
+            "password": "Userpassword#2345"
+        }
+
+        self.new_user = user.User(
+            username="crimson",
+            password="Userpassword#2345",
+            email="crimson@gmail.com")
+
+        self.data = {
+            "email": "crimson@gmail.com",
+            "password": "Userpassword#2345"
+        }
         self.new_user.save()
-        self.new_mentor = user.User("Turing", "Inigma", "allan@gmail.com")
+        self.new_mentor = user.User("Turing", "Inigma1#UKT", "allan@gmail.com")
         self.new_mentor.save()
-        self.new_follower = user.User("Knuth", "log2", "donald@gmail.com")
+        self.new_follower = user.User("Knuth", "Log2#XUKT", "donald@gmail.com")
 
         self.new_bucket = bucketlist.Bucket("Travelling")
         self.new_bucket.save()
@@ -28,18 +44,20 @@ class BaseTestCase(TestCase):
         self.new_item1 = bucketlist.Item("Go to Mombasa")
         self.new_item1.save()
 
-        self.new_profile = user.Profile("@crimson_profile")
+        self.new_profile = profile.Profile("@crimson_profile", self.new_user)
         self.new_profile.save()
 
         # Assemble all componets
-        self.new_profile.owner = self.new_user
         self.new_bucket.items[self.new_item.name] = self.new_item
         self.new_bucket.items[self.new_item1.name] = self.new_item1
         self.new_bucket.save()
         self.new_profile.bucket_lists[self.new_bucket.name] = self.new_bucket
-        self.new_profile.mentors[self.new_mentor.user_name] = self.new_mentor
+        self.new_profile.mentors[self.new_mentor.username] = self.new_mentor
         self.new_profile.save()
+
         self.client = self.app.test_client()
+        self.login_url = "/api/v1.0/login/"
+        self.register_url = "/api/v1.0/register/"
 
     def tearDown(self):
         database.session.remove()
